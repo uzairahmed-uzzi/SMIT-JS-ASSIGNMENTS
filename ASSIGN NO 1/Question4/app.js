d32d8788d30e87040de63ab9fe879a16d9ba517b
Uzair Ahmed <64686086+uzairahmed-uzzi@users.noreply.github.com>
2023-04-07 20:10:17 +0500

    Add files via upload

--- ASSIGN NO 1/Question4/app.js
+++ ASSIGN NO 1/Question4/app.js
@@ -1,7 +1,7 @@
 for(let i=0;i<=15;i++){
     if(i%2==0){
-        console.log(`${i} IS EVEN`);
+        document.write(`${i} IS EVEN <br>`);
     }else{
-        console.log(`${i} IS ODD`);
+        document.write(`${i} IS ODD <br>`);
     }
 }
